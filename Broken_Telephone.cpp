c22953d6b64b1c6c80bdfa2d91c193f8f33c8788
Sameer Kumar <sameer2001kr@gmail.com>
2023-01-01 12:03:56 +0530

    one commit

---
+++ Broken_Telephone.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int x;
+        cin >> x;
+        int arr[x];
+        for(int i=0; i<x; i++){
+            cin >> arr[i];
+        }
+        int i=0;
+        int c = 0;
+        while(i<x){
+            if(i==0){
+                if(arr[i] != arr[i+1])
+                    c++;
+            }
+            else if(i==x-1){
+                if(arr[i]!=arr[i-1])
+                    c++;
+            }
+            else{
+                if(arr[i]!=arr[i+1] || arr[i]!=arr[i-1])
+                    c++;
+            }
+            i++;
+        }
+        cout << c << endl;
+    }
+    return 0;
+}
